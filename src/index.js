c20c78ec264b0ba2f3cb0f470c945a6da0ccb364
sokra89 <90367407+sokra89@users.noreply.github.com>
2021-10-10 20:09:40 -0500

    Se agrega conexión a la base de datos
    Se agrega conexión a la base de datos Mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb+srv://pruebasancestra:ancestra123@cluster0.xcdgq.mongodb.net/dbpruebasancestra?retryWrites=true&w=majority')
+     .then(db => console.log('Base ok'))   
+     .catch(err => console.error(err));  
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/tasks', require('./routes/tasks'));
+
+//Static Files
+app.use(express.static(__dirname + '\\public'));
+
+//Sever is listening
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
