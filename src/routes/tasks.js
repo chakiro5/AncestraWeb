2d3faed534cc5bbe8cb2b1836a3a6cecb02f22b7
sokra89 <90367407+sokra89@users.noreply.github.com>
2021-10-10 21:23:21 -0500

    Datos desde cliente
    Se agrega funcionalidad para recibir datos desde el cliente

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Tarea')
+const Task = require('../models/Task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    res.json('Se cargaron datos desde el cliente');
 });
 
 module.exports = router;
