b540712537868055428323394022b49081475606
sokra89 <90367407+sokra89@users.noreply.github.com>
2021-10-12 22:54:38 -0500

    Se agrega de registro desde frontend
    Se agrega registro de información desde el frontend  con Vuejs formulario registro de producto

---
+++ src/app/components/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">Productos Ancestra WEB</a>
+        </nav>
+        <div class="container pt-5">
+            <form @submit.prevent="addProducto">
+                <label>Nombre</label>
+                <div class="form-group">
+                        <input type="text" v-model="producto.nombreProducto" placeholder="">
+                    </div>
+                <label>Link Imagen</label>
+                <div class="form-group">
+                    <input type="text" v-model="producto.linkimagenProducto" placeholder="">
+                    </div>
+                <label>Descripción</label>
+                <div class="form-group">
+                        <input type="text" v-model="producto.descripcionProducto" placeholder="">
+                    </div>
+                <label>Precio</label>
+                <div class="form-group">
+                        <input type="text" v-model="producto.precioProducto" placeholder="">
+                    </div>
+                <label>Vendedor</label>
+                <div class="form-group">
+                        <input type="text" v-model="producto.informacionVededor" placeholder="">
+                    </div>
+                <label>Observaciones</label>
+                <div class="form-group">
+                        <input type="text" v-model="producto.observaciones" placeholder="">
+                    </div>
+                <div class="form-group pt-2">
+                        <button class="btn btn-primary">REGISTRAR</button>
+
+                    </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            producto:{
+                    nombreProducto: '',
+                    linkimagenProducto: '',
+                    descripcionProducto: '',
+                    precioProducto: '',
+                    informacionVededor: '',
+                    observaciones: ''
+            }
+        }
+    },
+    methods:{
+        addProducto(){
+            console.log(this.producto)
+        }
+    }
+}
+</script>
